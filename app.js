47a7862bed6569d01e5900c3b2a67dae472760a5
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-17 12:02:25 +0000

    replaced .send to .json to respond to the client with the json object

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).send("Hello from the server side");
+  res.status(200).json({ message: "Hello from the server side" });
 });
 
 const port = 3000;
