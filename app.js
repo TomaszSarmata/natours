0c441d4800025c79df25dcece5aa3183c860194f
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-17 12:07:20 +0000

    Set up another endpoint to accept post requests to the root route

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
     .json({ message: "Hello from the server side", app: "Natours" });
 });
 
+app.post("/", (req, res) => {
+  res.status(200).json({ message: "post method works now" });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
