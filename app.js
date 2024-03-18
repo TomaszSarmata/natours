b81659774ab475c1b68fad31d1750f2494f4bfa7
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-18 14:57:17 +0000

    Commented out some code

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ message: "Hello from the server side", app: "Natours" });
-});
+// app.get("/", (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: "Hello from the server side", app: "Natours" });
+// });
 
-app.post("/", (req, res) => {
-  res.status(200).json({ message: "post method works now" });
-});
+// app.post("/", (req, res) => {
+//   res.status(200).json({ message: "post method works now" });
+// });
 
 const port = 3000;
 app.listen(port, () => {
