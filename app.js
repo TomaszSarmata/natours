c1d00f4105eafa7bfb415ba638f3dda2be078734
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-19 15:53:01 +0000

    Made sure the req.param.id gets converted into a number

--- app.js
+++ app.js
@@ -34,7 +34,9 @@ app.get("/api/v1/tours", (req, res) => {
 app.get("/api/v1/tours/:id", (req, res) => {
   console.log(req.params);
 
-  const tour = tours.find((el) => el.id === req.params);
+  const id = req.params.id * 1;
+
+  const tour = tours.find((el) => el.id === id);
 
   res.status(200).json({
     status: "success",
