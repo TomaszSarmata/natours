77ebeab0d5591801147cb5991df255a5e4e338cb
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-19 16:00:28 +0000

    Added a simple sanity check to make sure the id from the req.param.id is withing the range of tours

--- app.js
+++ app.js
@@ -36,6 +36,13 @@ app.get("/api/v1/tours/:id", (req, res) => {
 
   const id = req.params.id * 1;
 
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
   const tour = tours.find((el) => el.id === id);
 
   res.status(200).json({
