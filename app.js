b946262461376fe151d595fa86af6d63dfdf0595
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-20 15:19:12 +0000

    Add delete route handler just to test the api connection

--- app.js
+++ app.js
@@ -96,6 +96,22 @@ app.patch("/api/v1/tours/:id", (req, res) => {
   });
 });
 
+//adding the route handler for delete request
+app.delete("/api/v1/tours/:id", (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(204).json({
+    status: "success",
+    message: "tour successfully deleted",
+    data: null,
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
