74329f398fcb142cb4a6357543c0a99505c48f93
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-23 16:51:02 +0000

    appended tourController to each handler/controller in the tour routers

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,8 +3,15 @@ const tourController = require("./../controllers/tourController");
 
 const router = express.Router();
 
-router.route("/").get(getAllTours).post(createTour);
+router
+  .route("/")
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
 
-router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
+router
+  .route("/:id")
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
