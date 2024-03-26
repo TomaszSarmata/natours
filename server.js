25544e3f585885a8a35388348ad28268d4bf43b6
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-26 15:02:43 +0000

    Added alternative port to the server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ dotenv.config({ path: "./config.env" }); //this line will allow our program to r
 
 console.log(process.env);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
