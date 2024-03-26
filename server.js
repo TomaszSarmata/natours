7fe632d73491633f0e913804f32adf9c4fe585e0
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-26 15:04:57 +0000

    code clean up

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require("./app");
 
 dotenv.config({ path: "./config.env" }); //this line will allow our program to read the file and save to variables as environment variables
 
-console.log(process.env);
+// console.log(process.env);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
