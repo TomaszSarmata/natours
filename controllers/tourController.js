88c3cad3ff9aa6619944e5e3569055bd94057f89
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-29 18:30:12 +0000

    got rid off some code related to tour object coming from reading the json file with tours. As now we gonna read it from db directly and not from the static files we had to refactore the code

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,16 +1,4 @@
-const fs = require("fs");
 const Tour = require("../models/tourModel");
-
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is:, ${val}`);
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
-  next();
-};
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
@@ -27,10 +15,10 @@ exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
     requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours: tours,
-    },
+    // results: tours.length,
+    // data: {
+    //   tours: tours,
+    // },
   });
 };
 
@@ -39,39 +27,23 @@ exports.getTour = (req, res) => {
 
   const id = req.params.id * 1;
 
-  const tour = tours.find((el) => el.id === id);
+  // const tour = tours.find((el) => el.id === id);
 
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour: tour,
-    },
-  });
+  // res.status(200).json({
+  //   status: "success",
+  //   data: {
+  //     tour: tour,
+  //   },
+  // });
 };
 
 exports.createTour = (req, res) => {
-  // console.log(req.body);
-
-  const newId = tours[tours.length - 1].id + 1; //creating the new id for the new entry;
-  //below creating our entry object that will be data in the req.body and the newId - we have to merge both and so we use Object.assign
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  //here we are simply pushing that new entry into our list of tours
-  tours.push(newTour);
-
-  //now we have to persist new entry
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: "success",
-        data: {
-          tour: newTour,
-        },
-      });
-    },
-  );
+  res.status(201).json({
+    status: "success",
+    // data: {
+    //   tour: newTour,
+    // },
+  });
 };
 
 exports.updateTour = (req, res) => {
