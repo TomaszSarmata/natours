3b19f171833d0f84fab767823bb0bd0922258324
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-29 17:39:51 +0000

    Got rid off the test tour instance

--- server.js
+++ server.js
@@ -15,40 +15,6 @@ mongoose
   })
   .then(() => console.log("DB connections successfull!"));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "A tour must have a name"],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, "A tour must have a price"],
-  },
-});
-
-// tour model below
-const Tour = mongoose.model("Tour", tourSchema);
-
-// console.log(process.env);
-
-const testTour = new Tour({
-  name: "The Park Camper",
-  // rating: 4.7,
-  price: 497,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => console.log("Error !!!", err));
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
