52c1265c17c744eb28fa9f0428baeebc25a533ee
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-30 17:46:37 +0000

    Implemented the update tour api controller using findByIdAndUpdate method passing the id from the req.params and the req.body that contains the new data that will be written and then some options to return the new object and to run the schema validators

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -56,13 +56,24 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour: "<Updated Tour here...>",
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour: "<Updated Tour here...>",
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "failed",
+      message: `Error! Couldn't update the value: ${err}`,
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
