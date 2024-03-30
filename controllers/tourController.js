661ce9fe5ff0baea56a76cfa3bb6bfe6333f2f3f
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-30 21:53:26 +0000

    added req.query to the .find on getAllTours to filter through the req.query object which allows a user to pass some query params

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,15 @@ const Tour = require("../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    console.log("here", req.query);
+
+    const tours = await Tour.find(req.query);
+
+    // const tours = await Tour.find()
+    //   .where("duration")
+    //   .equals(5)
+    //   .where("difficulty")
+    //   .equals("easy");
 
     res.status(200).json({
       status: "success",
