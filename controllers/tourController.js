c4a0532cc8da1cabebf84abb3aae12f63374cec4
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-04-01 09:43:13 +0100

    Created a new file apiFeatures.js in the utils folder and moved our apiFeatures class in there

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require("../models/tourModel");
+const APIFeatures = require("../utils/apiFeatures");
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = "5";
@@ -7,61 +8,61 @@ exports.aliasTopTours = (req, res, next) => {
   next();
 };
 
-class APIFeatures {
-  constructor(query, queryString) {
-    this.query = query;
-    this.queryString = queryString;
-  }
+// class APIFeatures {
+//   constructor(query, queryString) {
+//     this.query = query;
+//     this.queryString = queryString;
+//   }
 
-  filter() {
-    // 1a) Filtering
-    const queryObj = { ...this.queryString };
-    const excludedFields = ["page", "sort", "limit", "fields"];
-    excludedFields.forEach((el) => delete queryObj[el]);
+//   filter() {
+//     // 1a) Filtering
+//     const queryObj = { ...this.queryString };
+//     const excludedFields = ["page", "sort", "limit", "fields"];
+//     excludedFields.forEach((el) => delete queryObj[el]);
 
-    // 1b) Advanced Filtering
+//     // 1b) Advanced Filtering
 
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+//     let queryStr = JSON.stringify(queryObj);
+//     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    // let query = Tour.find(JSON.parse(queryStr));
-    this.query = this.query.find(JSON.parse(queryStr));
+//     // let query = Tour.find(JSON.parse(queryStr));
+//     this.query = this.query.find(JSON.parse(queryStr));
 
-    return this;
-  }
-  //Sorting
+//     return this;
+//   }
+//   //Sorting
 
-  sort() {
-    if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.split(",").join(" ");
-      this.query = this.query.sort(sortBy);
-    } else {
-      this.query = this.query.sort("-createdAt");
-    }
-    return this;
-  }
-  // 3) Field limiting
+//   sort() {
+//     if (this.queryString.sort) {
+//       const sortBy = this.queryString.sort.split(",").join(" ");
+//       this.query = this.query.sort(sortBy);
+//     } else {
+//       this.query = this.query.sort("-createdAt");
+//     }
+//     return this;
+//   }
+//   // 3) Field limiting
 
-  limitFields() {
-    if (this.queryString.fields) {
-      const fields = this.queryString.fields.split(",").join(" "); //without this
-      this.query = this.query.select(fields);
-    } else {
-      this.query = this.query.select("-__v");
-    }
-    return this;
-  }
-  // 4) Pagination
+//   limitFields() {
+//     if (this.queryString.fields) {
+//       const fields = this.queryString.fields.split(",").join(" "); //without this
+//       this.query = this.query.select(fields);
+//     } else {
+//       this.query = this.query.select("-__v");
+//     }
+//     return this;
+//   }
+//   // 4) Pagination
 
-  paginate() {
-    const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.limit * 1 || 100;
-    const skip = (page - 1) * limit;
+//   paginate() {
+//     const page = this.queryString.page * 1 || 1;
+//     const limit = this.queryString.limit * 1 || 100;
+//     const skip = (page - 1) * limit;
 
-    this.query = this.query.skip(skip).limit(limit);
-    return this;
-  }
-}
+//     this.query = this.query.skip(skip).limit(limit);
+//     return this;
+//   }
+// }
 
 exports.getAllTours = async (req, res) => {
   try {
